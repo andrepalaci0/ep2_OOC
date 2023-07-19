60f4058ebcc3f1417db010575650c455efbac0ce
palaci0 <palaci0@pop-os.localdomain>
2023-07-19 20:20:08 -0300

    Adiciona o inicio do uso de Strategy pros algoritmos de ordenaçao

---
+++ codigoNovo/SortingAlgoQuickSort.java
@@ -0,0 +1,73 @@
+public class SortingAlgoQuickSort implements SortingAlgoStrategy {
+    public int particiona(int ini, int fim) {
+        if (ini < fim) {
+
+            int q = particiona(ini, fim);
+
+            ordena(ini, q);
+            ordena(q + 1, fim);
+        }
+        return 0;
+    }
+    //obviamente esses algoritmos tem q ser adaptados p receber a collection que, teoricamente
+    //vai permitir um acesso a essas variaveis
+    //nao sei preciso pensar ainda - itu
+
+    public void ordena(int ini, int fim) {
+
+        Produto x = produtos[ini];
+        int i = (ini - 1);
+        int j = (fim + 1);
+
+        while (true) {
+
+            if (criterio.equals(CRIT_DESC_CRESC)) {
+
+                do {
+                    j--;
+
+                } while (produtos[j].getDescricao().compareToIgnoreCase(x.getDescricao()) > 0);
+
+                do {
+                    i++;
+
+                } while (produtos[i].getDescricao().compareToIgnoreCase(x.getDescricao()) < 0);
+            } else if (criterio.equals(CRIT_PRECO_CRESC)) {
+
+                do {
+                    j--;
+
+                } while (produtos[j].getPreco() > x.getPreco());
+
+                do {
+                    i++;
+
+                } while (produtos[i].getPreco() < x.getPreco());
+            }
+
+            else if (criterio.equals(CRIT_ESTOQUE_CRESC)) {
+
+                do {
+                    j--;
+
+                } while (produtos[j].getQtdEstoque() > x.getQtdEstoque());
+
+                do {
+                    i++;
+
+                } while (produtos[i].getQtdEstoque() < x.getQtdEstoque());
+
+            } else {
+
+                throw new RuntimeException("Criterio invalido!");
+            }
+
+            if (i < j) {
+                Produto temp = produtos[i];
+                produtos[i] = produtos[j];
+                produtos[j] = temp;
+            } else
+                return j;
+        }
+    }
+}
---
+++ codigoNovo/SortingAlgoStrategy.java
@@ -0,0 +1,5 @@
+interface SortingAlgoStrategy
+{
+    public int particiona(int ini, int fim);
+    public void ordena(int ini, int fim);
+}
